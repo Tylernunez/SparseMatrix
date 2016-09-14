7ae64c4fd59a89209331203785a893e7f80dea07
Tyler Nunez <tyler.nunez@selu.edu>
2016-09-14 12:33:00 -0500

    Added Insert and print function to ValueNode, should test for accuracy

--- SparseMatrix/ValueNode.cs
+++ SparseMatrix/ValueNode.cs
@@ -11,5 +11,24 @@ namespace SparseMatrix
         private int row;
         private int column;
         private int value;
+
+        public Node insertFront(Node node, HeadNode head)
+        {
+
+            node.next = head;
+            return node;
+        }
+        public void print(HeadNode head)
+        {
+            if(head == null)
+            {
+                return;
+            }
+            do
+            {
+                System.Console.WriteLine(node.value);
+                node = node.next;
+            } while (node != head);
+        }
     }
 }
